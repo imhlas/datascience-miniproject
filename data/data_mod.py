d79ad7ddb6a5477664428154aaf71c269bbde4df
Nella <nella.nieminen@gmail.com>
2024-09-25 10:07:52 +0300

    Adding default values and more correct filtering in data_mod.py

--- data/data_mod.py
+++ data/data_mod.py
@@ -1,22 +1,40 @@
 import pandas as pd
 
 class AlcoholDataset:
-    def __init__(self, dataset_name) -> pd.DataFrame:
+    def __init__(self, dataset_name, dataset_group, ages = None, frequences = None) -> pd.DataFrame:
+        self.dataset_group = dataset_group
         self.dataset_name = dataset_name
         self.invalid_geo = [
             'European Union - 27 countries (from 2020)',
             'European Union - 28 countries (2013-2020)',
             'Italy'
         ]
-        self.ages = [
-            'From 15 to 24 years',
-            'From 25 to 34 years',
-            'From 35 to 44 years',
-            'From 45 to 54 years',
-            'From 55 to 64 years',
-            'From 65 to 74 years',
-            '75 years or over'
-        ]
+
+        # Default value for ages
+        if ages == None:
+            self.ages = [
+                'From 15 to 24 years',
+                'From 25 to 34 years',
+                'From 35 to 44 years',
+                'From 45 to 54 years',
+                'From 55 to 64 years',
+                'From 65 to 74 years',
+                '75 years or over'
+            ]
+        else:
+            self.ages = ages
+        
+        # Default value for frequences
+        if frequences == None:
+            self.frequences = ['Every day',
+                                'Every month',
+                                'Every week',
+                                'Less than once a month',
+                                'Never or not in the last 12 months']
+        else:
+            self.frequences = frequences
+
+ 
         self.raw_data = self.get_eurostat_datasets(dataset_name)
         self.cleaned_data = self.clean_data()
         self.filtered_data = self.data_filtering(self.cleaned_data)
@@ -36,21 +54,34 @@ class AlcoholDataset:
         return dataset
 
     def data_filtering(self, dataset):
+        
+        condition = None
+        if self.dataset_group == 'education':
+            condition = (dataset['isced11'] != 'All ISCED 2011 levels')
+        
         dataset = dataset[
                     (dataset['age'].isin(self.ages))
-                    & (~dataset['frequenc'].isin(self.invalid_geo))
+                    & (~dataset['geo'].isin(self.invalid_geo))
                     & (dataset['sex'] != 'Total')
-                    & (dataset['isced11'] != 'All ISCED 2011 levels')
-                    & (~dataset['frequenc'].isin(['Never', 'Not in the last 12 months']))
+                    & (dataset['frequenc'].isin(self.frequences))
+                    & condition
                     ]
 
         return dataset
         
     def freq_to_columns(self, dataset):
-        self.filtered_data = dataset.pivot_table(index=['isced11','age', 'geo','sex', 'TIME_PERIOD'],  
+        
+        index_list = ['age', 'geo','sex', 'TIME_PERIOD']
+        if self.dataset_group == 'education':
+            index_list.append('isced11')
+        
+        self.filtered_data = dataset.pivot_table(index=index_list, 
                                                   columns='frequenc',           
                                                   values='OBS_VALUE',                
                                                   aggfunc='sum')              
 
         return self.filtered_data.reset_index()
+
+    def get_dataset(self):
+        return self.pivot_dataset
     
