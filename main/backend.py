f39d2d9592f4ae261b3cba3ea54ce0634840d76b
Lucas <lucas.decabolozano@helsinki.fi>
2024-10-26 15:02:59 +0300

    Docstring for backend file

--- main/backend.py
+++ main/backend.py
@@ -4,11 +4,37 @@ import clustering
 from os import path
 
 def write_dataset(data, folder, dataset_name):
+    """
+    Helper function to write datasets from memory to folders.
+    
+    Parameters:
+    - data (Pandas dataframe): The dataframe to save
+    - folder (String): Target folder path where the dataframe is saved
+    - dataset_name (String): Name for the dataset
+    
+    Outcome:
+    - It creates a file as specified by the parameters
+    """
+    
     filepath=path.join(folder, dataset_name + ".csv")
     data.to_csv(filepath, sep=";")
     print("Writing ", dataset_name, " to ", filepath)
 
 def get_clusters(dataset_name, dataset_group, force_download_flag=False, rewrite_clean_datasets=True, rewrite_clusters=True):
+    """
+    Function that loads data using the data_load.py file, cleans the data using the data_mod.py file, and performs the clustering using the clustering.py file.
+    
+    Parameters:
+    - dataset_name (String): It is "hlth_ehis_al1e", "hlth_ehis_al1i" or "hlth_ehis_al1u".
+    - dataset_group (String): It is "education", "income" or "urbanisation".
+    - force_download_flag (bool): Defaults to False. If True it attempts to load the dataset from the internet instead of from local.
+    - rewrite_clean_data (bool): Defaults to True, in that case, any changes done to the data_mod.py file will overwrite the files in datasets_clean
+    - rewrite_clusters (bool): Defaults to True, in that case, the clusters are also overwriten
+    
+    Outcome:
+    - Prints the status in console, and saves the files to folders according to the flags.
+    """
+    
     print("Starting the processing...")
     data = data_load.load_datasets(dataset_name = dataset_name, force_download_flag=force_download_flag)
 
@@ -34,6 +60,9 @@ def get_clusters(dataset_name, dataset_group, force_download_flag=False, rewrite
 
 
 if __name__ == "__main__":
+    """
+    Calls the get_clusters() function to obtain the education, income and urbanisation clusters and output them to a folder
+    """
     get_clusters(dataset_name='hlth_ehis_al1e', dataset_group='education')
     get_clusters(dataset_name='hlth_ehis_al1i', dataset_group='income')
     get_clusters(dataset_name='hlth_ehis_al1u', dataset_group='urbanisation')
